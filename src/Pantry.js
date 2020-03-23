cc8d4fa3860e140043ca3b8fff2195327d530fb6
Joe Haefling <josephhaefling@gmail.com>
2020-03-23 16:59:59 -0600

    Build out pantry class

---
+++ src/Pantry.js
@@ -0,0 +1,20 @@
+class Pantry {
+  constructor(userIngredients, allIngredients) {
+    this.userIngredients = userIngredients;
+    this.allIngredients = allIngredients;
+  }
+
+  checkPantryForIngredients() {
+    //check to see if user has enough ingredients to cook recipe
+  }
+
+  findNeededIngredients() {
+    // find what ingredients are necessary to cook recipe
+  }
+
+  removeRecipeIngredients() {
+    //remove ingredients from pantry once recipe has been started
+  }
+}
+
+module.exports = Pantry
