3f53c8f203d7ce7a1e6ce33f5ca6f6d761ff8ffc
Joe Haefling <josephhaefling@gmail.com>
2020-03-23 19:53:47 -0600

    Set up user test and Pantry test

--- src/User.js
+++ src/User.js
@@ -8,4 +8,6 @@ class User {
   }
 }
 
-module.exports = User;
+if (typeof module !== 'undefined') {
+  module.exports = User;
+}
--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -5,7 +5,7 @@ const Pantry = require('../src/Pantry');
 
 describe('Pantry', function() {
   it('should be a function', function() {
-    pantry = new Pantry({recipe: 23423432, image: "https://spoonacular.com/recipeImages/595736-556x370.jpg", ingredients:  });
+    pantry = new Pantry();
 
     expect(Pantry).to.be.a('function')
   })
