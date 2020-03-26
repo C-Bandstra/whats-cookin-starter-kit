14fc1ea8586a7bd0fab33dcd27d7ccd9f3956457
Charlie Bandstra <bandstracharlie@gmail.com>
2020-03-26 17:14:10 -0600

    Add functionality to update array methods

--- src/User.js
+++ src/User.js
@@ -6,6 +6,25 @@ class User {
     this.favoriteRecipes = [];
     this.recipesToCook = [];
   }
+  updateFavorites(recipe) {
+    if (!this.favoriteRecipes.includes(recipe)) {
+      this.favoriteRecipes.push(recipe)
+    } else {
+      let match = this.favoriteRecipes.filter(recipe => clickedRecipe = recipe)
+      let index = this.favoriteRecipes.indexOf(match)
+      this.favoriteRecipes.splice(index)
+    }
+  }
+
+  updateRecipesToCook(recipe) {
+    if (!this.recipesToCook.includes(recipe)) {
+      this.recipesToCook.push(recipe)
+    } else {
+      let match = this.recipesToCook.filter(recipe => clickedRecipe = recipe)
+      let index = this.recipesToCook.indexOf(match)
+      this.recipesToCook.splice(index)
+    }
+  }
 }
 
 if (typeof module !== 'undefined') {
