03bba988b99292b221fa39bbf0a0ae99625e9a63
Charlie Bandstra <bandstracharlie@gmail.com>
2020-03-29 14:23:32 -0600

    change class name

--- src/domInsertions.js
+++ src/domInsertions.js
@@ -7,10 +7,10 @@ let domInsertions = {
       </section>
       <img id="${recipe.id}"src=${recipe.image} class="recipe-img">
       <section id="${recipe.id}" class="card-button-container">
-        <button class="favorite-button">
+        <button class="recipe-button">
           <img id="${recipe.name}" src="https://i.ya-webdesign.com/images/heart-icon-png-7.png" class="favorite-img">
         </button>
-        <button class="add-recipe-button">
+        <button class="recipe-button">
           <img id="${recipe.name}" src="https://image.flaticon.com/icons/svg/32/32339.svg" class="add-recipe-img">
         </button>
       </section>
