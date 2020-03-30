e528162aa9392e4a0d0b02157c1cf71dddc1fb74
Charlie Bandstra <bandstracharlie@gmail.com>
2020-03-30 15:22:51 -0600

    Move code, add cook-btn functionality

--- src/scripts.js
+++ src/scripts.js
@@ -44,16 +44,27 @@ function clickHandler() {
     displayPage(instructionsPage, addedPage, homePage, favoritesPage)
     displayInstructions(User.recipesToCook);
   }
+  if(classList.contains('cook-btn')) {
+    displayCookMessage();
+  }
 }
 
-function displayInstructions(arr) {
-  let clickedRecipe = arr.filter(recipe => recipe.id == event.target.id)
-  let index = arr.indexOf(clickedRecipe[0])
-  let instructions = arr[index].getInstructions()
-  instructionsHeader.innerText = `${clickedRecipe[0].name}`
-  instructionsContainer.innerHTML = `<button class="cook-btn">Cook This Recipe!</button>`;
-  instructions.forEach((inst, i) => instructionsContainer.insertAdjacentHTML('beforeend', 
-  `<p class="instruction-step">Step ${i + 1}: ${inst.instruction}</p>`))
+function loadPage() {
+  showRecipes(recipes);
+  generateUser();
+}
+
+function showRecipes() {
+  recipeData.forEach(recipe => {    
+    allCardsContainer.insertAdjacentHTML('beforeend', domInsertions.insertRecipeCard(recipe))
+  });
+}
+
+
+function generateUser() {
+  let user = usersData[Math.floor(Math.random() * usersData.length)]
+  let pantry = new Pantry(user.pantry)
+  User = new User(user.id, user.name, pantry)
 }
 
 function displayUserRecipes(arr) {
@@ -63,6 +74,28 @@ function displayUserRecipes(arr) {
     currentPage[0].insertAdjacentHTML('beforeend', domInsertions.insertRecipeCard(recipe))
   });
   event.target.classList.contains('favorites-button') ? changeFavCards() : changeSavedCards();
+}
+
+function displayIconChange(active, inactive) {
+  event.target.src === active ? event.target.src = inactive : event.target.src = active;
+}
+
+function addToUser() {
+  let clickedRecipe = recipes.filter(recipe => {
+    return event.target.id === recipe.name
+  });
+  return clickedRecipe[0]
+}
+
+function displayPage(currentPage, page1, page2, page3) {
+  currentPage.removeAttribute('hidden');
+  page1.setAttribute('hidden', '')
+  page2.setAttribute('hidden', '');
+  page3.setAttribute('hidden', '');
+  page1.childNodes[3].classList.remove('current')
+  page2.childNodes[3].classList.remove('current')
+  page3.childNodes[3].classList.remove('current')
+  currentPage.childNodes[3].classList.add('current')
 }
 
 function changeFavCards() {
@@ -77,40 +110,24 @@ function changeSavedCards() {
   buttons.forEach(button => button.innerHTML = `<p id="${button.id}" class="saved instructions-btn">Get Instructions</p>`)
 }
 
-function loadPage() {
-  showRecipes(recipes);
-  generateUser();
+function displayInstructions(arr) {
+  let clickedRecipe = arr.filter(recipe => recipe.id == event.target.id)
+  let index = arr.indexOf(clickedRecipe[0])
+  let instructions = arr[index].getInstructions()
+  instructionsHeader.innerText = `${clickedRecipe[0].name}`
+  instructionsContainer.innerHTML = `<button id="${event.target.id}" class="cook-btn">Cook This Recipe!</button>`;
+  instructions.forEach((inst, i) => instructionsContainer.insertAdjacentHTML('beforeend', 
+  `<p class="instruction-step">Step ${i + 1}: ${inst.instruction}</p>`))
 }
 
-function showRecipes() {
-  recipeData.forEach(recipe => {    
-    allCardsContainer.insertAdjacentHTML('beforeend', domInsertions.insertRecipeCard(recipe))
-  });
-}
-
-function generateUser() {
-   let user = usersData[Math.floor(Math.random() * usersData.length)]
-   User = new User(user.id, user.name, user.pantry)
-}
-
-function displayIconChange(active, inactive) {
-  event.target.src === active ? event.target.src = inactive : event.target.src = active;
-}
-
-function displayPage(currentPage, page1, page2, page3) {
-  currentPage.removeAttribute('hidden');
-  page1.setAttribute('hidden', '')
-  page2.setAttribute('hidden', '');
-  page3.setAttribute('hidden', '');
-  page1.childNodes[3].classList.remove('current')
-  page2.childNodes[3].classList.remove('current')
-  page3.childNodes[3].classList.remove('current')
-  currentPage.childNodes[3].classList.add('current')
-}
-
-function addToUser() {
-  let clickedRecipe = recipes.filter(recipe => {
-    return event.target.id === recipe.name
-  });
-  return clickedRecipe[0]
+function displayCookMessage() {
+  let recipe = recipeData.filter(recipe => recipe.id == event.target.id);
+  let needed = User.pantry.findNeededIngredients(recipe[0]);
+  let cookBtn = instructionsContainer.getElementsByClassName('cook-btn')
+  cookBtn[0].insertAdjacentHTML('afterend', `<div class="needed-list"></div>`)
+  let list = instructionsContainer.getElementsByClassName('needed-list')
+  needed.forEach(item => {
+    list[0].insertAdjacentHTML('afterbegin', 
+    `<li>You need ${item.amountNeeded} ${item.quantity.unit} of ${item.name}</li>`)
+  })
 }
